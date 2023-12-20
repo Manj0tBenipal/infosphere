eed8169f19ce847dd84a7c12b1d42c985fb80ebe
Manj0tBenipal <manjotbenipal00001@gmail.com>
2023-12-20 12:37:58 -0500

    Added View for article page which  displays article in  full length

--- app/news/article/page.tsx
+++ app/news/article/page.tsx
@@ -1,3 +1,5 @@
+import FullArticle from "@/public/types/News";
+import styles from "@/styles/news.module.css";
 export default async function page({
   searchParams,
 }: {
@@ -24,12 +26,51 @@ export default async function page({
     url += `?ref=headlines`;
   }
   url += `&articleId=${searchParams.articleId}`;
-  const res = await fetch(url, { next: { revalidate: 1 } });
+  const res = await fetch(url, { next: { revalidate: 0 } });
   const data = await res.json();
-  console.log(data);
+  const article = data.data as FullArticle;
+  //Splitting the paragraph returned by API into lines
+  const lines: string[] = article.content.split(".");
+  //Keeping tracks of lines joined into paragraphs
+  let coveredTillLine = 0;
+  //Generating a paragraph from every 4 lines
+  let paras: string[] = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    if (i > 0 && i % 3 === 0) {
+      paras.push(lines.slice(coveredTillLine, i).join(". ")); //Joining the lines into a paragraph
+      coveredTillLine = i;
+    }
+  }
+  //Adding the remaining lines as a paragraph
+  paras.push(lines.slice(coveredTillLine, lines.length).join(". "));
+
+  if (!data.found) return <h1>The article has been moved or is deleted</h1>;
   return (
-    <article>
-      <h1 className="fontXL"></h1>
-    </article>
+    <main
+      className={`${styles.newsWrapper} flex flex-center flex-column flex-gap-1`}
+    >
+      <h1 className="fontL">{article.title}</h1>
+      <img src={article.img} alt="Image" className={`${styles.coverImage}`} />
+      <p className={`italic`}>
+        <i>
+          &quot;
+          {article.description}&quot;
+        </i>
+      </p>
+
+      <article className={`${styles.article}`}>
+        <span>
+          {`${article.creator ? article.creator : ""} 
+        ${article.pubDate} `}
+          :
+          {paras.map((para: string, i: number) => (
+            <p className={`${styles.newsPara}`} key={i}>
+              {para}
+            </p>
+          ))}
+        </span>
+      </article>
+    </main>
   );
 }
