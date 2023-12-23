60e5d88e18b61cfcda6e54d9056d01eb1d48a13f
Manj0tBenipal <manjotbenipal00001@gmail.com>
2023-12-23 12:21:31 -0500

    Added viewport in meta for media queries to work properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body suppressHydrationWarning={true}>
-       
-
         <p className="ud">
           <IoMdWarning />
           This Project is still in Development
