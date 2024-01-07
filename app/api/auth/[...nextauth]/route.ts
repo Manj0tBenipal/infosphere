f0e6375bc2970ffdb3cba99160220ea4c94011dc
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-01-07 11:47:04 -0500

    Created a Globally accessible authOptions object to be imported in api routes and views(SessionProvider)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,6 @@
+import { authOptions } from "@/config/nextauth";
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-import { cert } from "firebase-admin/app";
-import type { Adapter } from "next-auth/adapters";
-const handler = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-    }),
-  ],
-  adapter: FirestoreAdapter({
-    credential: cert({
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-      projectId: process.env.FIREBASE_PROJECT_ID,
-    }),
-  }) as Adapter,
-});
+
+
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
