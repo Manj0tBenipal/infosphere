7600f5b6f524a2884476aee5f7cadc0affb9da2f
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-01-15 09:04:25 -0500

    Adapted: MUI button styles for services section

--- components/home/Service.tsx
+++ components/home/Service.tsx
@@ -1,5 +1,6 @@
 import { Service } from "@/public/types/Service";
 import styles from "@/styles/service.module.css";
+import { Button } from "@mui/joy";
 import Image from "next/image";
 import Link from "next/link";
 export default function ServiceSection({ service }: { service: Service }) {
@@ -8,11 +9,11 @@ export default function ServiceSection({ service }: { service: Service }) {
       className={`${styles.service} flex width-full flex-column flex-center text-center flex-gap-small`}
     >
       <Image src={service.image} height={150} width={150} alt="service" />
-      <div className={`${styles.serviceText}`}>
+      <div className={`${styles.serviceText}  flex flex-column flex-gap-small`}>
         <h3>{service.name}</h3>
         <p>{service.details}</p>
-        <Link href={service.link} className="btn-primary">
-          Explore
+        <Link href={service.link}>
+          <Button className="btn-primary">Explore</Button>
         </Link>
       </div>
     </div>
