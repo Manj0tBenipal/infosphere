e4445f6fc32eb1a544e23e372d51d26522889e08
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-01-18 09:25:10 -0500

    enh: Added an  auth check before creating a new document in firestore and a redirect in case the user is not logged in

--- app/guides/NewGuideButton.tsx
+++ app/guides/NewGuideButton.tsx
@@ -3,23 +3,39 @@
 import { generateNewArticle } from "@/lib/syncArticle";
 import { API_RES } from "@/public/types/API";
 import { Button } from "@mui/joy";
-import { usePathname, useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { redirect, usePathname, useRouter } from "next/navigation";
 
 export default function NewGuideButton() {
   const router = useRouter();
   const pathname = usePathname();
+  const { data, status } = useSession();
   return (
     <Button
-      onClick={async () => {
-        const res: API_RES = JSON.parse(await generateNewArticle());
-        if (res.success) {
-          const searchParams = new URLSearchParams();
-          searchParams.append("aID", res.res.id);
-          router.push(`${pathname}/create?${searchParams.toString()}`);
-        } else {
-          alert(res.err);
+      onClick={
+        /**
+         * This function created a new document in firestore and redireacts
+         * to create page where the id is used for changing data of the document
+         */
+        async () => {
+          /**
+           * New Guide is only created if the user is signed in otherwise
+           * the  user is redirected to signin page
+           */
+          if (status === "authenticated") {
+            const res: API_RES = JSON.parse(await generateNewArticle());
+            if (res.success) {
+              const searchParams = new URLSearchParams();
+              searchParams.append("aID", res.res.id);
+              router.push(`${pathname}/create?${searchParams.toString()}`);
+            } else {
+              alert(res.err);
+            }
+          } else {
+            return redirect("/api/auth/signin");
+          }
         }
-      }}
+      }
       className="btn-dark"
       href={`/guides/create`}
     >
