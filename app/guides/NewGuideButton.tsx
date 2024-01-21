c85eaef10bd65a92e9c9b6070386aeb5ef41bb1a
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-01-20 23:28:06 -0500

    enh: editor opens in a new tab. Button text changes as the request is made to create a new Guide

--- app/guides/NewGuideButton.tsx
+++ app/guides/NewGuideButton.tsx
@@ -5,13 +5,21 @@ import { API_RES } from "@/public/types/API";
 import { Button } from "@mui/joy";
 import { useSession } from "next-auth/react";
 import { redirect, usePathname, useRouter } from "next/navigation";
-
+import { useState } from "react";
+interface ButtonProperties {
+  text: string;
+  disabled: boolean;
+}
 export default function NewGuideButton() {
-  const router = useRouter();
+  const [buttonProp, setButtonProp] = useState<ButtonProperties>({
+    text: "Create a new Guide",
+    disabled: false,
+  });
   const pathname = usePathname();
   const { data, status } = useSession();
   return (
     <Button
+      disabled={buttonProp.disabled}
       onClick={
         /**
          * This function created a new document in firestore and redireacts
@@ -23,11 +31,16 @@ export default function NewGuideButton() {
            * the  user is redirected to signin page
            */
           if (status === "authenticated") {
+            setButtonProp({ text: "Creating...", disabled: true });
             const res: API_RES = JSON.parse(await generateNewArticle());
             if (res.success) {
               const searchParams = new URLSearchParams();
               searchParams.append("aID", res.res.id);
-              router.push(`${pathname}/create?${searchParams.toString()}`);
+              setButtonProp({ text: "Create a new Guide", disabled: false });
+              window.open(
+                `${pathname}/create?${searchParams.toString()}`,
+                "_blank"
+              );
             } else {
               alert(res.err);
             }
@@ -39,7 +52,7 @@ export default function NewGuideButton() {
       className="btn-dark"
       href={`/guides/create`}
     >
-      Create a new Guide
+      {buttonProp.text}
     </Button>
   );
 }
