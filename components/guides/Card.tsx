cde49271643e46762ffa6d1f9ef22bb453494f75
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-01-21 12:23:57 -0500

    refactor: newsCard class included in the parent Link tag to add desired behavior of in-line block element

--- components/guides/Card.tsx
+++ components/guides/Card.tsx
@@ -5,8 +5,8 @@ import { ReactElement } from "react";
 import newsStyles from "@/styles/news.module.css";
 export default function CardComponent({ data }: { data: Guide }): ReactElement {
   return (
-    <Link href={`/guides?gID=${data.id}`}>
-      <Card variant="outlined" className={newsStyles.newsCard}>
+    <Link href={`/guides?gID=${data.id}`} className={newsStyles.newsCard}>
+      <Card variant="outlined">
         <AspectRatio minHeight={150} maxHeight={200}>
           <img
             src={data.img?.url || process.env.IMAGE_NOT_FOUND_URL}
@@ -20,7 +20,7 @@ export default function CardComponent({ data }: { data: Guide }): ReactElement {
               ? data.title.slice(0, 80) + "..."
               : data.title}
           </Typography>
-        
+
           <Typography level="body-sm">{data.userId}</Typography>
           <Typography level="body-xs">{data.date}</Typography>
         </div>
